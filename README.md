124dfe188a751dac832e9188fad2f7264d55ad9a
XXX YYY <cartman07@mail.ru>
2022-04-06 00:56:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# jest-interactive-mode !!! POC !!!
+
+emacs jest tool with immediately highlighting results
+
+![](./jest-interactive-mode.gif)
--- jest-interactive-mode.el
+++ jest-interactive-mode.el
@@ -1,23 +1,8 @@
-;;; jest-interactive-mode --- a simple package                     -*- lexical-binding: t; -*-
+;;; jest-interactive-mode.el --- Jest interactive mode  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014  Nic Ferrier
-
-;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
+;; URL: https://github.com/i-have-no-name/jest-interactive-mode
 ;; Keywords: lisp
 ;; Version: 0.0.1
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -28,88 +13,137 @@
 (require 'json)
 (require 'filenotify)
 
-(defun jim/open()
+(defun jest-interactive--replace-msg (str)
+  (replace-regexp-in-string "\s+"
+                            " "
+                            (replace-regexp-in-string "\x1b\[[0-9;]*m"
+                                                      "" str)))
+
+(defun jest-interactive--get_message (msg)
+  (let* ((lines (split-string msg "\n"))
+         (expected (jest-interactive--replace-msg (nth 2 lines)))
+         (received (jest-interactive--replace-msg (nth 3 lines))))
+    (concat expected ", " received)))
+
+(defun jest-interactive--expect_message (msg)
+  (save-match-data (and (string-match "\(\\(.+?\\):\\([0-9]+\\):\\([0-9]+\\)\)"
+                                      msg)
+                        (let* ((line (match-string 2 msg))
+                               (_ (goto-line (string-to-number line)))
+                               (p1 (point))
+                               (__ (end-of-line))
+                               (p2 (point))
+                               (ov (make-overlay p1 p2 nil t)))
+                          (push ov overlays)
+                          (overlay-put ov
+                                       'after-string
+                                       (propertize (concat " "
+                                                           (jest-interactive--get_message msg))
+                                                   'face
+                                                   '(error)))
+                          (overlay-put ov
+                                       'before-string
+                                       (propertize "x"
+                                                   'display
+                                                   '(left-fringe filled-rectangle error)))))))
+
+
+(defun jest-interactive--success (ov)
+  (overlay-put ov
+               'before-string
+               (propertize "x"
+                           'display
+                           '(left-fringe filled-rectangle success))))
+
+(defun jest-interactive--failure (ov msg)
+  (overlay-put ov
+               'before-string
+               (propertize "x"
+                           'display
+                           '(left-fringe filled-rectangle error)))
+  (jest-interactive--expect_message msg))
+
+
+(defun jest-interactive--create-empty-file-if-no-exists (filePath)
+  (unless (file-exists-p filePath)
+    (with-temp-buffer
+      (write-file filePath)))
+  (write-region "{\"testResults\": [{\"assertionResults\": []}] }"
+                nil filePath))
+
+
+(defun jest-interactive--run ()
+  (message "jest-interactive-mode processing")
+  (let* ((json-object-type 'hash-table)
+         (json-array-type 'list)
+         (json-key-type 'string)
+         (json (json-read-file jest-interactive--results-file-name))
+         (results (gethash "testResults" json))
+         (head (car results))
+         (asserts (gethash "assertionResults" head)))
+    (unless (null overlays)
+      (dolist (ov overlays)
+        (delete-overlay ov)))
+    (dolist (assert asserts)
+      (let* ((title (gethash "title" assert))
+             (status (gethash "status" assert))
+             (msgs (gethash "failureMessages" assert)))
+        (save-excursion
+          (goto-char 1)
+          (let ((position (search-forward-regexp (format "test('%s'\\|it('%s'" title title))))
+            (end-of-line)
+            (let ((ov (make-overlay position
+                                    (point)
+                                    nil
+                                    t)))
+              (push ov overlays)
+              (if (string= status "passed")
+                  (jest-interactive--success ov)
+                (jest-interactive--failure ov
+                                           (car msgs))))))))))
+
+(defun jest-interactive--open ()
   (interactive)
+  (message "jest-interactive-mode enabled")
   (set (make-local-variable 'overlays)
        '())
-  (set (make-local-variable 'jim/results-file-name) (concat (file-name-directory buffer-file-name) "__jest_interactive_mode_results__.json"))
-
-  (defun jim/run()
-    (defun get_message(msg)
-(defun jest-interactive--replace-msg(str)
-(replace-regexp-in-string "\s+" " " ( replace-regexp-in-string "\x1b\[[0-9;]*m" "" str )))
-(let* ((lines (split-string msg "\n"))
-(expected ( jest-interactive--replace-msg (nth 2 lines) ))
-(received ( jest-interactive--replace-msg (nth 3 lines) )))
-(concat expected ", " received)))
-    (defun expect_message(msg)
-(save-match-data (and (string-match "\(\\(.+?\\):\\([0-9]+\\):\\([0-9]+\\)\)" msg)
-(let ((line (match-string 2 msg)))
-(goto-line (string-to-number line))
-(let ((p1 (point)))
-(end-of-line)
-(let ((p2 (point)))
-(let ((ov (make-overlay p1 p2 nil t)))
-(push ov overlays)
-(overlay-put ov 'after-string (propertize (concat " " (get_message msg)) 'face
-'(error)))
-(overlay-put ov 'before-string (propertize "x" 'display '(left-fringe filled-rectangle error))))))))))
-    (let* ((json-object-type 'hash-table)
-           (json-array-type 'list)
-           (json-key-type 'string)
-           (json (json-read-file jim/results-file-name))
-           (results (gethash "testResults" json))
-           (head (car results))
-           (asserts (gethash "assertionResults" head)))
-      (unless (null overlays)
-        (dolist (tm overlays)
-          (delete-overlay tm)))
-      (dolist (assert asserts)
-(let* ((title (gethash "title" assert))
-(status (gethash "status" assert))
-(msgs (gethash "failureMessages" assert)))
-(defun success(ov)
-(overlay-put ov 'before-string (propertize "x" 'display '(left-fringe filled-rectangle success))))
-(defun failure(ov msg)
-(overlay-put ov 'before-string (propertize "x" 'display '(left-fringe filled-rectangle error)))
-;; (overlay-put ov 'after-string (propertize " failure" 'face '(error)))
-            (expect_message msg))
-(save-excursion (goto-char 1)
-(let ((position (search-forward-regexp (format "test('%s'\\|it('%s'" title title))))
-(end-of-line)
-(let ((ov (make-overlay position (point) nil t)))
-(push ov overlays)
-(if (string= status "passed")
-(success ov)
-(failure ov (car msgs))))))))))
-  (defun jim/create-empty-file-if-no-exists(filePath)
-(if (file-exists-p filePath)
-()
-(with-temp-buffer (write-file filePath)))
-(write-region "{\"testResults\": [{\"assertionResults\": []}] }" nil filePath))
-  (jim/create-empty-file-if-no-exists jim/results-file-name)
+  (set (make-local-variable 'jest-interactive--results-file-name)
+       (concat (file-name-directory buffer-file-name)
+               "__jest_interactive_mode_results__.json"))
+  (jest-interactive--create-empty-file-if-no-exists
+   jest-interactive--results-file-name)
   (set (make-local-variable 'fd)
-       (file-notify-add-watch jim/results-file-name '(change)
+       (file-notify-add-watch jest-interactive--results-file-name
+                              '(change)
                               (lambda (event)
-                                (jim/run))))
+                                (jest-interactive--run))))
   (set (make-local-variable 'process)
-       (make-comint-in-buffer "jest-interactive" nil "yarn" nil "jest" (buffer-file-name) "--json"
-                              (format "--outputFile=%s" jim/results-file-name)
-                               "--watch"))
-  (jim/run))
+       (make-comint-in-buffer "jest-interactive"
+                              nil
+                              "yarn"
+                              nil
+                              "jest"
+                              (buffer-file-name)
+                              "--json"
+                              (format "--outputFile=%s" jest-interactive--results-file-name)
+                              "--watch"))
+  (jest-interactive--run))
 
-(defun jim/close()
+(defun jest-interactive--close ()
   (delete-process process)
   (kill-buffer process)
   (file-notify-rm-watch fd)
-  (delete-file jim/results-file-name)
-  (dolist (tm overlays)
-    (delete-overlay tm)))
+  (delete-file jest-interactive--results-file-name)
+  (dolist (ov overlays)
+    (delete-overlay ov))
+  (message "jest-interactive-mode disabled"))
 
-(define-minor-mode jest-interactive-mode "jest interactive mode"
+(define-minor-mode jest-interactive-mode
+  "jest interactive mode"
   :ligher "jest-interactive"
-  (if jest-interactive-mode (jim/open)
-    (jim/close)))
+  (if jest-interactive-mode
+      (jest-interactive--open)
+    (jest-interactive--close)))
 
 
 (provide 'jest-interactive-mode)
